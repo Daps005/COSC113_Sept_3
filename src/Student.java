4b287c89c2d56c8a9449388acfca233415c847d9
Heritage Awogbade <haawogbade@upei.ca>
2026-09-03 09:58:50 -0400

    Overloaded Constructor - 2

--- src/Student.java
+++ src/Student.java
@@ -31,5 +31,19 @@ public class Student {
         System.out.println(s1);
         System.out.println(s1.FN);
 
+        Student s2 = new Student ("Tobiloba", "Ayodeji", 14141);
+        System.out.println(s2.FN);
+        System.out.println(s2.LN);
+
+
+        Student s3;
+        String FN = "Miguel";
+        String LN = "Gascaortega";
+        int Sid = 31415;
+        s3 = new Student (FN, LN,Sid);
+        System.out.println(s3.FN);
+        System.out.println(s3.LN);
+
+
     }
 }
