72158b56ec6ac48273de597520d621d72e08605a
Heritage Awogbade <haawogbade@upei.ca>
2026-09-03 09:40:56 -0400

    Overloaded Constructor - 1

---
+++ src/Student.java
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Student {
+    // Attributes
+    String FN;
+    String LN;
+    int Sid;
+
+    // Default constructor
+    Student(){
+        this.FN = "No first name";
+        this.LN = "No last name";
+        this.Sid = 0;
+
+
+    }
+
+    Student(String FN, String LN, int Sid) {
+        this.FN = FN;
+        this.LN = LN;
+        this.Sid = Sid;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        // Variable s1 is of Student type
+        Student s1;
+        s1 = new Student();
+        System.out.println(s1);
+        System.out.println(s1.FN);
+
+    }
+}
